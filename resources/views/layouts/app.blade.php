00d5bf456f4c13a18df1a53da913d4f87d8bd464
sonic mc <allysonmachaka@gmail.com>
2025-11-20 16:10:52 +0200

    ug

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -874,11 +874,15 @@
           <div class="social-icons" style="margin-top: 24px;">
             <a href="#" aria-label="Twitter"><i class="fab fa-twitter"></i></a>
             <a href="https://www.instagram.com/queenofpeacerehab?igsh=MTNxb2o2d2Zwb3lsdQ%3D%3D&utm_source=qr" 
-            target="_blank" 
-            aria-label="Instagram">
-             <i class="fab fa-instagram"></i>
-         </a>
-            <a href="#" aria-label="Facebook"><i class="fab fa-facebook-f"></i></a>
+                target="_blank" 
+                aria-label="Instagram">
+                <i class="fab fa-instagram"></i>
+            </a>
+            <a href="https://www.facebook.com/profile.php?id=61584069418559" 
+                aria-label="Facebook" 
+                target="_blank">
+                  <i class="fab fa-facebook-f"></i>
+              </a>
             <a href="#" aria-label="YouTube"><i class="fab fa-youtube"></i></a>
           </div>
         </div>
